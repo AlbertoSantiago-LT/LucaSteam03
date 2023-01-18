1f13d2aaf20c3143f1fef72c07553a25bb3d84a3
VLRuben <valinoruben@gmail.com>
2023-01-18 17:59:42 +0100

    Corregidos 4 bugs

--- main.py
+++ main.py
@@ -1,31 +1,44 @@
 import os
+import time
 import functions
 import validaciones.validacion as validacion
 
+seguir = True
+
 def menu():
-    print("\nLucaSteam03 ")
-    print("**********************************************")
-    print("                                             *")
-    print("Elige una opción del menú                    *")
-    print("1. Dar de alta un juego                      *")
-    print("2. Listado general de juegos                 *")
-    print("3. Listado de juegos de la consola Nintendo  *")
-    print("4. Listado de juegos del género Plataforma   *")
-    print("                                             *")
-    print("**********************************************")
-   
-    #data = obtener_datos()
-    #opcion = input("Seleccione una opcion: ")
-    opcion = validacion.is_numero_positivo("Seleccione una opcion: ")
-    if opcion == 1:
-        functions.alta_juego()
-    elif opcion == 2:
-        print(functions.listado_juegos())
-    elif opcion == 3: 
-        print(functions.listado_nintendo())
-    elif opcion == 4:
-        print(functions.listado_plataforma())
-    else:
-        print("El numero no corresponde a ninguna accion")
+    opcion = 0
+    while opcion!=6:
+        
+        os.system("cls")
+        print("\nLucaSteam03 ")
+        print("**********************************************")
+        print("                                             *")
+        print("Elige una opción del menú                    *")
+        print("1. Dar de alta un juego                      *")
+        print("2. Listado general de juegos                 *")
+        print("3. Listado de juegos de la consola Nintendo  *")
+        print("4. Listado de juegos del género Plataforma   *")
+        print("5. Mostrar juegos dados de alta              *")
+        print("6. Salir                                     *")
+        print("**********************************************")
+       
+        opcion = validacion.is_numero_positivo("Seleccione una opcion: ")
+        if opcion == 1:
+            functions.alta_juego()
+            functions.esperarInput()
+        elif opcion == 2:
+            print(functions.listado_juegos())
+            functions.esperarInput()
+        elif opcion == 3: 
+            print(functions.listado_nintendo())
+            functions.esperarInput()
+        elif opcion == 4:
+            print(functions.listado_plataforma())
+            functions.esperarInput()
+        elif opcion==6:
+            print("Cerrando LucaSteam03...")
+        else:
+            print("El numero no corresponde a ninguna accion, volviendo al menú")
+            time.sleep(2.5)
         
 if __name__ == "__main__": menu()
