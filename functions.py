f69e6af48d198bafdd738b6bc47cd3fb5ef4f0ac
VLRuben <valinoruben@gmail.com>
2023-01-18 09:27:28 +0100

    Commit 2 : Estructura general hecha
    Se hace la estructura general del proyecto, desde la que se va a partir para generar las funciones

--- functions.py
+++ functions.py
@@ -1,2 +1,11 @@
 def alta_juego(data):
-    return "hola"
+    return "Alta juego"
+
+def listado_juegos(data):
+    return "Listado juegos"
+
+def listado_nintendo(data):
+    return "Nintendo"
+
+def listado_plataforma(data):
+    return "Plataforma"
