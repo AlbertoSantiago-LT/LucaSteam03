aa10a91190385308082f307b1a040c49f9bcb633
Ripday18 <kevinvillacres97@gmail.com>
2023-01-19 12:34:19 +0100

    cambios en el main

--- main.py
+++ main.py
@@ -10,42 +10,6 @@ seguir = True
 def menu():
     opcion = -1
     while opcion!=0:
-<<<<<<< HEAD
-=======
-        
-        os.system("cls")
-        f = Figlet(font='slant')
-        print(Fore.GREEN + f.renderText("LucaSteam03"))
-        print("**********************************************")
-        print("                                             *")
-        print(Fore.BLUE + "Elige una opción del menú                    *")
-        print(Fore.YELLOW + "1. Dar de alta un juego                      *")
-        print(Fore.YELLOW + "2. Listado general de juegos                 *")
-        print(Fore.YELLOW + "3. Listado de juegos de la consola Nintendo  *")
-        print(Fore.YELLOW + "4. Listado de juegos del género Plataforma   *")
-        print(Fore.YELLOW + "5. Mostrar juegos dados de alta              *")
-        print(Fore.YELLOW + "6. Salir                                     *")
-        print(Fore.RESET + "**********************************************")
-       
-        opcion = validacion.is_numero_positivo("Seleccione una opcion: ")
-        if opcion == 1:
-            functions.alta_juego()
-            functions.esperarInput()
-        elif opcion == 2:
-            print(functions.listado_juegos())
-            functions.esperarInput()
-        elif opcion == 3: 
-            print(functions.listado_nintendo())
-            functions.esperarInput()
-        elif opcion == 4:
-            print(functions.listado_plataforma())
-            functions.esperarInput()
-        elif opcion==0:
-            print("Cerrando LucaSteam03...")
-        else:
-            print("El numero no corresponde a ninguna accion, volviendo al menú")
-            time.sleep(2.5)
->>>>>>> cc2d547ef99d5fd480e9c8a9013b11ce49e45951
         
         os.system("cls")
         f = Figlet(font='slant')
@@ -70,8 +34,6 @@ def menu():
             print("El numero no corresponde a ninguna accion, volviendo al menú")
             time.sleep(1.5)
         
-        
-        
 def menu_listados():
     opcion = -1
     while opcion!=0:
@@ -84,7 +46,7 @@ def menu_listados():
         print(Fore.YELLOW + "2. Listado de juegos de la consola Nintendo  *")
         print(Fore.YELLOW + "3. Listado de juegos del género Plataforma   *")
         print(Fore.YELLOW + "4. Listado de juegos dados de alta           *")
-        print(Fore.YELLOW + "4. Listado de juegos dado un rango a elegir  *")
+        print(Fore.YELLOW + "5. Listado de juegos dado un rango a elegir  *")
         print(Fore.YELLOW + "5. Listado de desarrolladores de los juegos  *")
         print(Fore.YELLOW + "6. Listado filtrado por siglo                *")
         print(Fore.YELLOW + "7. Listado filtrado por género               *")
@@ -104,12 +66,15 @@ def menu_listados():
             print(functions.listado_alta_juegos())
             functions.esperarInput() 
         elif opcion == 5:
-            print(functions.listar_desarrolladores())
+            print(functions.listado_rango())
             functions.esperarInput()  
         elif opcion == 6:
+            print(functions.listar_desarrolladores())
+            functions.esperarInput()  
+        elif opcion == 7:
             print(functions.listado_siglo())
             functions.esperarInput() 
-        elif opcion == 7:
+        elif opcion == 8:
             print(functions.listado_genero())
             functions.esperarInput() 
         elif opcion == 0:
