e5a828c1b03bfed58c2f139f7c488ff3ac028173
Ripday18 <kevinvillacres97@gmail.com>
2023-01-20 08:30:26 +0100

    pruebas unitarias
    pruebas unitarias

--- unitest_listajuegios.py
+++ unitest_listajuegios.py
@@ -3,23 +3,16 @@ import pandas as pd
 import functions as f
 
 class TestsLucaSteam03(unittest.TestCase):
-
-
+    
     # def test_listado_nintendo(self):
     #     data = pd.DataFrame()
     #     data = f.listado_nintendo()
     #     self.assertTrue("Nintendo" in data["Publisher"].values)
-
-
-
-    # def test_genero_plataforma(self):
-    #     data = pd.DataFrame()
-    #     data = f.listado_platforma()
-    #     self.assertTrue("Plataform" in data["Genre"].values)
-
-    def test_listado_segun_rango(self):
+        
+    def test_listado_plataforma(self):
         data = pd.DataFrame()
-        data = f.listado_rango()
-        self.assertTrue(data)
+        data = f.listado_plataforma()
+        self.assertTrue("Platform" in data["Genre"].values)
+                   
 
 if __name__ == '__main__' : unittest.main()
