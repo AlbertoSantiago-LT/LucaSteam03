33f6332e0d8521c30d1aca6a6e340260ba6de78e
Ripday18 <kevinvillacres97@gmail.com>
2023-01-20 08:39:35 +0100

    New test
    New test

--- unitest_listajuegios.py
+++ unitest_listajuegios.py
@@ -9,10 +9,22 @@ class TestsLucaSteam03(unittest.TestCase):
     #     data = f.listado_nintendo()
     #     self.assertTrue("Nintendo" in data["Publisher"].values)
         
-    def test_listado_plataforma(self):
-        data = pd.DataFrame()
-        data = f.listado_plataforma()
-        self.assertTrue("Platform" in data["Genre"].values)
-                   
+    # def test_listado_plataforma(self):
+    #     data = pd.DataFrame()
+    #     data = f.listado_plataforma()
+    #     self.assertTrue("Platform" in data["Genre"].values)
+
+    def test_validar_fecha(self):
+        # Prueba con una fecha válida
+        fecha = 1995
+        resultado = f.validar_fecha()
+        self.assertEqual(fecha, resultado)
+
+        # Prueba con una fecha inválida (menor a 1952)
+        fecha = 1950
+        resultado = f.validar_fecha()
+        self.assertNotEqual(fecha, resultado)    
+
+        
 
 if __name__ == '__main__' : unittest.main()
